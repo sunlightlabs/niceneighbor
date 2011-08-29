c88e0567cd37bf849ba06414c395bc47c6d507d7
Dan Drinkard <dan.drinkard@gmail.com>
2011-08-29 11:00:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Niceneighbor::Application.config.secret_token = '32fb360948b7045df0158bc57ea32cd1477eacd14793ca01c081fcebcf806090af716b8ec5f7d94b9eb618c1000b76d5ba7af4d60b4558c4b9aae1c70917d484'
