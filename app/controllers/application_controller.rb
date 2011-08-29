213877f75697cc5f360ddf8223c8a882b5fd6e06
Dan Drinkard <dan.drinkard@gmail.com>
2011-08-29 17:42:58 -0400

    restructure settings.yml to stand in for ENV

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def settings
-    @@settings ||= YAML.load_file "#{Rails.root}/config/settings.yml"
+    @@settings ||= (YAML.load_file "#{Rails.root}/config/settings.yml") || ENV
   end
 
 end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,9 +1,9 @@
-settings = YAML.load_file "#{Rails.root}/config/settings.yml"
+settings = (YAML.load_file "#{Rails.root}/config/settings.yml") || ENV
 
 RailsAdmin.config do |config|
   config.authenticate_with do
     authenticate_or_request_with_http_basic do |user_name, password|
-      user_name == settings[:rails_admin][:username] && password == settings[:rails_admin][:password]
+      user_name == settings['RAILS_ADMIN_USERNAME'] && password == settings['RAILS_ADMIN_PASSWORD']
     end
   end
 end
