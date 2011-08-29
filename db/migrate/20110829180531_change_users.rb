d27e7cb147c9ac029ecb7bcd0ac1c0caa6001371
Dan Drinkard <dan.drinkard@gmail.com>
2011-08-29 17:56:19 -0400

    convert phone to string

--- db/migrate/20110829180531_change_users.rb
+++ db/migrate/20110829180531_change_users.rb
@@ -2,7 +2,7 @@ class ChangeUsers < ActiveRecord::Migration
   def self.up
     change_table :users do |t|
       t.string  :username, :limit => 16
-      t.integer :phone, :limit => 10
+      t.string  :phone, :limit => 10
       t.boolean :voice_only
       t.string  :profession
       t.text    :profile
