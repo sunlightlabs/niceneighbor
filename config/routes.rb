32a925e83ad564e360e4a68a9398b1c6de409aea
Luigi Montanez <luigi.montanez@gmail.com>
2011-08-30 14:37:42 -0400

    Add username field to registration process

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Niceneighbor::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users do
+    get "sign_in", :to => "devise/sessions#new"
+    get "sign_up", :to => "devise/registrations#new"
+    get "sign_out", :to => "devise/sessions#destroy"
+  end
 
   match 'voice' => 'twilio#voice'
   match 'sms' => 'twilio#sms'
