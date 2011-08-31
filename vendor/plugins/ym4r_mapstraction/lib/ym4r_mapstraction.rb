b63f069233dc6aa718fc5fa1d3c693c18c3741a3
Dan Drinkard <dan.drinkard@gmail.com>
2011-08-31 02:03:44 -0400

    and the rest of them

--- vendor/plugins/ym4r_mapstraction/lib/ym4r_mapstraction.rb
+++ vendor/plugins/ym4r_mapstraction/lib/ym4r_mapstraction.rb
@@ -23,19 +23,22 @@ module Ym4r
     end
 
     unless File.exist?(RAILS_ROOT + '/config/map24_api_key.yml')
-      raise Map24APIKeyConfigFileNotFoundException.new("File RAILS_ROOT/config/map24_api_key.yml not found")
+      # raise Map24APIKeyConfigFileNotFoundException.new("File RAILS_ROOT/config/map24_api_key.yml not found")
+      MAP24_API_KEY = ENV['MAP24_API_KEY']
     else
       MAP24_API_KEY = YAML.load_file(RAILS_ROOT + '/config/map24_api_key.yml')[ENV['RAILS_ENV']]
     end
 
     unless File.exist?(RAILS_ROOT + '/config/mapquest_api_key.yml')
-      raise MapquestAPIKeyConfigFileNotFoundException.new("File RAILS_ROOT/config/mapquest_api_key.yml not found")
+      # raise MapquestAPIKeyConfigFileNotFoundException.new("File RAILS_ROOT/config/mapquest_api_key.yml not found")
+      MAPQUEST_API_KEY = ENV['MAPQUEST_API_KEY']
     else
       MAPQUEST_API_KEY = YAML.load_file(RAILS_ROOT + '/config/mapquest_api_key.yml')[ENV['RAILS_ENV']]
     end
 
     unless File.exist?(RAILS_ROOT + '/config/multimap_api_key.yml')
-      raise MultimapAPIKeyConfigFileNotFoundException.new("File RAILS_ROOT/config/multimap_api_key.yml not found")
+      # raise MultimapAPIKeyConfigFileNotFoundException.new("File RAILS_ROOT/config/multimap_api_key.yml not found")
+      MULTIMAP_API_KEY = ENV['MULTIMAP_API_KEY']
     else
       MULTIMAP_API_KEY = YAML.load_file(RAILS_ROOT + '/config/multimap_api_key.yml')[ENV['RAILS_ENV']]
     end
