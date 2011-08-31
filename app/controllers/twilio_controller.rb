a3d92408246c7dee0345fdbd7c2d3c243d621adb
Luigi Montanez <luigi.montanez@gmail.com>
2011-08-31 15:12:30 -0400

    SMS: List messages

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -15,6 +15,7 @@ class TwilioController < ApplicationController
     state = params['FromState']
     zip = params['FromZip']
 
+    @sms = []
     # Do we know this user?
     if user = User.find_by_phone(phone)
 
@@ -22,29 +23,37 @@ class TwilioController < ApplicationController
       if user.username == user.phone
         user.username = body
         user.save!
-        @payload = "Your username is #{user.username}. What's your street address and zip code?"
+        @sms << "Your username is #{user.username}. What's your street address and zip code?"
       elsif user.location.nil?
         geo = Geokit::Geocoders::MultiGeocoder.geocode(body)
         if geo.success
           user.update_attributes!(:location => geo.full_address.split(',')[0], :city => geo.city, :state => geo.state, :zip => geo.zip)
-          @payload = "Great. Account set up! To use, start a message with \"I need ...\" or \"I have ...\", or reply with \"List\" to see needs near you."
+          @sms << "Great. Account set up! To use, start a message with \"I need ...\" or \"I have ...\", or reply with \"List\" to see needs near you."
         else
-          @payload = "Couldn't figure out your address. Please reply with your FULL address."
+          @sms << "Couldn't figure out your address. Please reply with your FULL address."
         end
       elsif body =~ /need (.)/i
-        @payload = "Request received! We'll let your neighbors know about it."
+        @sms << "Request received! We'll let your neighbors know about it."
       elsif body =~ /have (.)/i
-        @payload = "Awesome. Thanks for letting us know!"
+        @sms << "Awesome. Thanks for letting us know!"
       elsif body =~ /list/i
-        @payload = "Thanks for checking in. Your neighbors don't need anything right now."
+
+        unless user.messages.empty?
+          user.messages.each_with_index do |msg, index|
+            @sms << "#{index + 1}. #{msg.activity.user.username}: #{msg.activity.title}"
+          end
+          @sms << 'Can you help with any? Reply with the number, along with a message for your neighbor.'
+        else
+          @sms << "Thanks for checking in. Your neighbors don't need anything right now."
+        end
       else
-        @payload = "I don't understand. Start a message with \"I need ...\" or \"I have ...\", or reply with \"List\" to see needs near you."
+        @sms << "I don't understand. Start a message with \"I need ...\" or \"I have ...\", or reply with \"List\" to see needs near you."
       end
 
     # Create a new user
     elsif !phone.nil?
       User.create!(:phone => phone, :email => phone + '@niceneighbor.net', :username => phone, :password => phone)
-      @payload = "Welcome to Nice Neighbor! What's a good username for you? Just letters and numbers, please."
+      @sms << "Welcome to Nice Neighbor! What's a good username for you? Just letters and numbers, please."
     end
 
   end
