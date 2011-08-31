14a5376323cc4f40487c4f3ece566334028ad5ff
Dan Drinkard <dan.drinkard@gmail.com>
2011-08-31 02:07:47 -0400

    catch unset vars, we are not going to add all of these

--- vendor/plugins/ym4r_mapstraction/lib/ym4r_mapstraction.rb
+++ vendor/plugins/ym4r_mapstraction/lib/ym4r_mapstraction.rb
@@ -17,28 +17,28 @@ module Ym4r
     unless File.exist?(RAILS_ROOT + '/config/gmaps_api_key.yml')
       # raise GMapsAPIKeyConfigFileNotFoundException.new("File RAILS_ROOT/config/gmaps_api_key.yml not found")
       # hacked for heroku
-      GMAPS_API_KEY = ENV['GMAPS_API_KEY']
+      GMAPS_API_KEY = ENV['GMAPS_API_KEY'] rescue ''
     else
       GMAPS_API_KEY = YAML.load_file(RAILS_ROOT + '/config/gmaps_api_key.yml')[ENV['RAILS_ENV']]
     end
 
     unless File.exist?(RAILS_ROOT + '/config/map24_api_key.yml')
       # raise Map24APIKeyConfigFileNotFoundException.new("File RAILS_ROOT/config/map24_api_key.yml not found")
-      MAP24_API_KEY = ENV['MAP24_API_KEY']
+      `MAP24_API_KEY` = ENV['MAP24_API_KEY'] rescue ''
     else
       MAP24_API_KEY = YAML.load_file(RAILS_ROOT + '/config/map24_api_key.yml')[ENV['RAILS_ENV']]
     end
 
     unless File.exist?(RAILS_ROOT + '/config/mapquest_api_key.yml')
       # raise MapquestAPIKeyConfigFileNotFoundException.new("File RAILS_ROOT/config/mapquest_api_key.yml not found")
-      MAPQUEST_API_KEY = ENV['MAPQUEST_API_KEY']
+      MAPQUEST_API_KEY = ENV['MAPQUEST_API_KEY'] rescue ''
     else
       MAPQUEST_API_KEY = YAML.load_file(RAILS_ROOT + '/config/mapquest_api_key.yml')[ENV['RAILS_ENV']]
     end
 
     unless File.exist?(RAILS_ROOT + '/config/multimap_api_key.yml')
       # raise MultimapAPIKeyConfigFileNotFoundException.new("File RAILS_ROOT/config/multimap_api_key.yml not found")
-      MULTIMAP_API_KEY = ENV['MULTIMAP_API_KEY']
+      MULTIMAP_API_KEY = ENV['MULTIMAP_API_KEY'] rescue ''
     else
       MULTIMAP_API_KEY = YAML.load_file(RAILS_ROOT + '/config/multimap_api_key.yml')[ENV['RAILS_ENV']]
     end
